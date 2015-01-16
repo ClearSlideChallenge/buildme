c97860c8d6256baa758a36bc3d0989b1e9f7eece
Michal Matyjek <mmatyjek@clearslide.com>
2015-01-16 14:52:15 -0800

    Adding new feature as requested by Product Management.

--- buildme/src/main/java/com/clearslideng/buildme/App.java
+++ buildme/src/main/java/com/clearslideng/buildme/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Blah variable = new Blah();
+    	System.out.println( "Hello World!" );
     }
 }
